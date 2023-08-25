f978ef61d77df0074f9ec3c56c576cc6b7e676dc
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-08-25 10:54:25 +0200

    Files

---
+++ GameEngineArchitecture/Assets/Scenes/MyCube.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCube : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+
+    private Camera _cam;
+
+    void Awake()
+    {
+        _cam = GameObject.FindObjectOfType<Camera>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal") * Time.deltaTime * _speed;
+        var v = Input.GetAxis("Vertical") * Time.deltaTime * _speed;
+        
+        transform.position += new Vector3(h, 0,  v);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
