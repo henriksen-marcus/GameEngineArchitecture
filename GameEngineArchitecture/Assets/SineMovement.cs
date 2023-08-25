c2eb910b15a6351425e18c1d0fda4f0f50bbce4a
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-08-25 11:11:52 +0200

    Update SineMovement.cs

--- GameEngineArchitecture/Assets/SineMovement.cs
+++ GameEngineArchitecture/Assets/SineMovement.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SineMovement : MonoBehaviour
 {
+
+    [SerializeField] private float amplitude = 1;
+    [SerializeField] private float waveSpeed = 2;
+    [SerializeField] private float movementSpeed = 0.5f;
+    private Vector3 _initialPosition;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var xPos = transform.position.x + movementSpeed * Time.deltaTime;
+        var yPos = _initialPosition.y + (Mathf.Sin(Time.realtimeSinceStartup * waveSpeed) * amplitude);
+        transform.position = new Vector3(xPos, yPos, _initialPosition.z);
     }
 }
