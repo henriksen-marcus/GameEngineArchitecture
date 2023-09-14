c2553512b81111f86bf4d81d3256dc8d741f48e2
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-09-14 09:46:38 +0200

    Pinballgame progress

--- GameEngineArchitecture/Assets/Scenes/PinballGame/Flipper.cs
+++ GameEngineArchitecture/Assets/Scenes/PinballGame/Flipper.cs
@@ -7,21 +7,31 @@ using UnityEngine.Serialization;
 public class Flipper : MonoBehaviour
 {
     [SerializeField] private AnimationCurve animationCurve;
-    [SerializeField] private float timer;
+    [SerializeField] private float _timer = 1f;
+    private Quaternion _initialRotation;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = gameObject.GetComponentInChildren<Rigidbody>();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            timer = 0;
+            _timer = 0;
         }
 
-        timer += Time.deltaTime;
+        _timer += Time.deltaTime;
         
         // Rot logic
-        var localRot = transform.localRotation;
-        var angle = animationCurve.Evaluate(timer);
-        localRot.z = angle;
-        transform.localRotation = localRot;
+        //var localRot = transform.localRotation;
+        //var angle = animationCurve.Evaluate(_timer);
+        //localRot.y = angle;
+        //transform.localRotation = localRot;
+
+        _rb.MoveRotation(_initialRotation * Quaternion.Euler(0, 0, animationCurve.Evaluate(_timer) * 90f));
+        //_rb.MoveRotation(_startRot * Quaternion.Euler(0, 0, animationCurve.Evaluate(_timer) * 90f));
     }
 }
