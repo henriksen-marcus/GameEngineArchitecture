0e0c3211c0d476c7a2b7819437020dfe76fd752e
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-09-17 17:03:04 +0200

    Flippers working

--- GameEngineArchitecture/Assets/Scenes/PinballGame/Flipper.cs
+++ GameEngineArchitecture/Assets/Scenes/PinballGame/Flipper.cs
@@ -6,32 +6,32 @@ using UnityEngine.Serialization;
 
 public class Flipper : MonoBehaviour
 {
+    [SerializeField] private KeyCode Button;
     [SerializeField] private AnimationCurve animationCurve;
     [SerializeField] private float _timer = 1f;
+    [SerializeField] private bool _isLeftFlipper;
     private Quaternion _initialRotation;
     private Rigidbody _rb;
 
     private void Start()
     {
         _rb = gameObject.GetComponentInChildren<Rigidbody>();
+        _initialRotation = transform.rotation;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(Button))
         {
             _timer = 0;
+            print("Reset");
         }
-
-        _timer += Time.deltaTime;
         
-        // Rot logic
-        //var localRot = transform.localRotation;
-        //var angle = animationCurve.Evaluate(_timer);
-        //localRot.y = angle;
-        //transform.localRotation = localRot;
-
-        _rb.MoveRotation(_initialRotation * Quaternion.Euler(0, 0, animationCurve.Evaluate(_timer) * 90f));
-        //_rb.MoveRotation(_startRot * Quaternion.Euler(0, 0, animationCurve.Evaluate(_timer) * 90f));
+        _timer += Time.deltaTime;
+    }
+    
+    private void FixedUpdate() {
+        int direction = _isLeftFlipper ? -1 : 1;
+        _rb.MoveRotation(_initialRotation * Quaternion.Euler(0,0,animationCurve.Evaluate(_timer) * 90f * direction));
     }
 }
