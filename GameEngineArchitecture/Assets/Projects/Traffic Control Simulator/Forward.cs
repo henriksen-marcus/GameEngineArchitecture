152c8d34b22e453796462572acec3fcd5d8d1bfc
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-11-10 14:55:49 +0100

    Auto acceleration and braking

--- GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Forward.cs
+++ GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Forward.cs
@@ -10,86 +10,67 @@ public class Forward : MonoBehaviour
     [SerializeField] private WheelCollider wheelColl;
     [SerializeField] private GameObject target;
     [SerializeField] private Rigidbody rigidbody;
+    /// <summary>
+    /// The minimum distance from an obstacle ahead before we start
+    /// emergency braking.
+    /// </summary>
     [SerializeField] private float minDistance = 5f;
+    /// <summary>
+    /// The minimum distance we want to keep from the car in front of us.
+    /// Should be higher than minDistance. This value will be overriden
+    /// to be higher when travelling at high speeds.
+    /// </summary>
+    [SerializeField] private float followDistance = 10f;
+    /// <summary>
+    /// The max braking acceleration we can apply to the car, for
+    /// example when emergency braking.
+    /// </summary>
+    [SerializeField] private float maxBrakeAcceleration = 5f;
     
-    
-    
-    private float _currentMotorTorque;
-    private float maxMotorTorque = 50f;
+    /// <summary>
+    /// The velocity of the car in the last FixedUpdate.
+    /// </summary>
     private Vector3 _lastVelocity;
-    private bool shouldDecel = false;
+    private bool shouldDecel = true;
     
     // Start is called before the first frame update
     void Start()
     {
         //Time.timeScale = 0.2f;
-        rigidbody.velocity = transform.forward * 13.8f;
-    }
-
-    void Decelerate()
-    {
+        rigidbody.velocity = transform.forward * 10f;
+        print(GetTargetAcceleration());
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Vector3 F = rigidbody.mass * GetAcceleration();
+            rigidbody.AddForce(-F);
+        }
     }
 
     private void FixedUpdate()
     {
+        Vector3 F = Vector3.zero;
         
-        // Now that we know the required force, we can apply it to the wheels
-        //float brakeTorque = F.magnitude * wheelColl.radius;
+        // Are we very close to the obstacle?
+        if (Vector3.Distance(transform.position, target.transform.position) < minDistance)
+        {
+            // Max brake
+            F = -rigidbody.velocity.normalized * maxBrakeAcceleration;
+            rigidbody.AddForce(F, ForceMode.Acceleration);
+        }
 
         if (shouldDecel)
         {
-            // Decelerate
-            Vector3 F = rigidbody.mass * GetAcceleration();
-            rigidbody.AddForce(-F);
+            // Decelerate, since we use ForceMode.Acceleration, we can just say F = a
+            F = GetTargetAcceleration();
+            rigidbody.AddForce(F, ForceMode.Acceleration);
         }
         
-        
-        _lastVelocity = rigidbody.velocity;
-        return;
-        var f = CalculateForce();
-        rigidbody.AddForce(f);
-        print(f);
-    
-    }
-
-    private Vector3 GetAcceleration()
-    {
-        return (rigidbody.velocity - _lastVelocity) / Time.fixedDeltaTime;
-    }
-
-    private float GetMass()
-    {
-        return rigidbody.mass;
-    }
-
-    private Vector3 CalculateForce()
-    {
-        // Gather required information for calculating acceleration
-        float v_0 = rigidbody.velocity.magnitude;
-        float v = 0; //target.GetComponent<Rigidbody>().velocity.magnitude;
-        float s = Vector3.Distance(transform.position, target.transform.position);
-        float m = rigidbody.mass;
-        print(s);
-        
-        if (s > minDistance)
-        {
-            // Obey speed limit
-            return transform.forward;
-        }
-        print("Calculating force");
-        
-        // The target acceleration needed to each the target position and speed
-        Vector3 a = (((v * v) - (v_0 * v_0)) / (2 * s)) * transform.forward;
-        Vector3 F = m * a;
-
-        return F;
         // We also need to know how far our brake distance is, take into account their braking distance as well!
 
         // Check if we should accelerate or decelerate
@@ -103,6 +84,69 @@ public class Forward : MonoBehaviour
         // We have found out the required force, now check if we need to apply it yet.
         // use SVT to calculate the time it takes to the target at our and their current velocity,
         // and check if this is less than 3 seconds (or the time set by the player)
+        
+        /*if (s > minDistance)
+        {
+            // Obey speed limit
+            return transform.forward;
+        }
+        print("Calculating force");*/
+        
+        // Now that we know the required force, we can apply it to the wheels
+        //float brakeTorque = F.magnitude * wheelColl.radius;
+        
+        _lastVelocity = rigidbody.velocity;
+    }
+
+    private Vector3 GetAcceleration()
+    {
+        return (rigidbody.velocity - _lastVelocity) / Time.fixedDeltaTime;
+    }
+
+    private float GetMass() => rigidbody.mass;
+
+    /// <returns>The closest position behind the car in front of us that we want to be.</returns>
+    private Vector3 GetFollowPosition()
+    {
+        var oPos = transform.position;
+        var tPos = target.transform.position;
+        return tPos - (oPos + tPos).normalized * followDistance;
+    }
+
+    private Vector3 GetTargetAcceleration()
+    {
+        // Gather required information for calculating acceleration
+        Vector3 velocity = rigidbody.velocity;
+        Vector3 oPos = transform.position; // Our
+        Vector3 tPos = target.transform.position;//GetFollowPosition(); // Target
+        float m = rigidbody.mass;
+        
+        // Calculate acceleration by using the kinematic equation for each vector component
+        
+        float v_0_x = velocity.x; // Starting speed
+        float v_x = 0; // Target speed
+        float s_x = MathF.Abs(oPos.x - tPos.x); // Distance to target
+        float a_x = s_x == 0f ? 0f : (v_x * v_x - v_0_x * v_0_x) / (2 * s_x); // Acceleration needed to reach target speed within s
+        
+        float v_0_y = velocity.y;
+        float v_y = 0;
+        float s_y = MathF.Abs(oPos.y - tPos.y);
+        float a_y = s_y == 0f ? 0f : (v_y * v_y - v_0_y * v_0_y) / (2 * s_y);
+        
+        float v_0_z = velocity.z;
+        float v_z = 0;
+        float s_z = MathF.Abs(oPos.z - tPos.z);
+        float a_z = s_z == 0f ? 0f : (v_z * v_z - v_0_z * v_0_z) / (2 * s_z);
+        
+        // The target acceleration needed to each the target position and speed
+        Vector3 a = new Vector3(a_x, a_y, a_z);
+        
+        /*print("v_x: " + v_x + " v_0_x: " + v_0_x + " s_x: " + s_x + " a_x: " + a_x);
+        print("v_y: " + v_y + " v_0_y: " + v_0_y + " s_y: " + s_y + " a_y: " + a_y);
+        print("v_z: " + v_z + " v_0_z: " + v_0_z + " s_z: " + s_z + " a_z: " + a_z);
+        print(a);*/
+        
+        return a;
     }
     
     private float GetTimeUntilTarget()
@@ -114,5 +158,6 @@ public class Forward : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + transform.forward * 1.5f);
+        Gizmos.DrawSphere(GetFollowPosition(), 1);
     }
 }
