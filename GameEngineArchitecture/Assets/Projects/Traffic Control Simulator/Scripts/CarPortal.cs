c09353855aaaa65794747ecfe9e72201d47fe596
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-12-06 21:30:58 +0100

    Add car portal script

---
+++ GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/CarPortal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles spawning and vanishing of cars at
+/// the edge of the map.
+/// </summary>
+public class CarPortal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
