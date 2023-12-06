91daba4628fc2021ca7c3bc60eb57087b8fd99d5
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-12-06 21:29:10 +0100

    Create IClickable

---
+++ GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/Interfaces/IClickable.cs
@@ -0,0 +1,10 @@
+
+public interface IClickable
+{
+
+    /// <summary>
+    /// Define behavior for when the object is clicked on by the player,
+    /// such as spawning a popup menu.
+    /// </summary>
+    public void OnClicked();
+}
