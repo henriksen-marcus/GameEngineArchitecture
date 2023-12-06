b241f46eff0968061846b9f4039253f05e195d78
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-12-06 21:28:12 +0100

    Create ArrowComponent

---
+++ GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/ArrowComponent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ArrowComponent : MonoBehaviour
+{
+    public Vector3 direction = Vector3.up;
+    [SerializeField] private float length = 1;
+    [SerializeField] private Vector3 offset = Vector3.zero;
+    [SerializeField] private Color color = Color.white;
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        var p = transform.position;
+        Gizmos.DrawLine(p + offset, p + offset + transform.InverseTransformDirection(direction) * length);
+        Gizmos.DrawWireSphere(p + offset, length * 0.05f);
+    }
+
+}
