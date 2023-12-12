a29425d57e6d5f5b316dd19ae87ee63bb532cef9
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-12-12 21:50:24 +0100

    Update IVehicle.cs

--- GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/Interfaces/IVehicle.cs
+++ GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/Interfaces/IVehicle.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public interface IVehicle
 {
+    /*WheelCollider frontLeftWheel { get; }
+    WheelCollider frontRightWheel { get; }
+    WheelCollider rearLeftWheel { get; }
+    WheelCollider rearRightWheel { get; }*/
+    
     public Rigidbody Rigidbody { get; }
     public BoxCollider Collider { get; }
     public Vector3 GetBootPosition();
