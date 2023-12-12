4c177f1275301eeacfbb435a249e38cdebdb7abe
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-12-12 16:31:54 +0100

    Car manager working as intended

--- GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/Managers/CarManager.cs
+++ GameEngineArchitecture/Assets/Projects/Traffic Control Simulator/Scripts/Managers/CarManager.cs
@@ -5,16 +5,49 @@ using UnityEngine;
 [RequireComponent(typeof(ObjectPooler))]
 public class CarManager : Singleton<CarManager>
 {
+    [SerializeField] private float spawnRate = 3f;
+    [SerializeField] private bool spawnAllOnStart = true;
+    [SerializeField] private CarSpawner[] spawners;
     protected ObjectPooler ObjectPooler;
+    private int _currentIndex = 0;
+    
+    public bool ShouldKeepSpawning { get; set; } = true;
     
     void Start()
     {
         ObjectPooler = GetComponent<ObjectPooler>();
         ObjectPooler.SetupPool();
+        
+        if (spawnAllOnStart)
+        {
+            for (int i = 0; i < spawners.Length; i++)
+                SpawnCar();
+        }
+        
+        StartCoroutine(WaitSpawnCar());
     }
     
-    void Update()
+    
+    IEnumerator WaitSpawnCar()
     {
+        while (ShouldKeepSpawning)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            SpawnCar();
+        }
+    }
+
+    void SpawnCar()
+    {
+        // Check if we can spawn a car
+        if (ObjectPooler.RemainingObjects <= 0 || !spawners[_currentIndex].CanSpawn()) return;
         
+        var car = ObjectPooler.GetObject();
+        var t = spawners[_currentIndex].SpawnPoint;
+        car.transform.position = t.position;
+        car.transform.rotation = t.rotation;
+        
+        // Change spawning point
+        _currentIndex = _currentIndex == spawners.Length - 1 ? 0 : _currentIndex + 1;
     }
 }
